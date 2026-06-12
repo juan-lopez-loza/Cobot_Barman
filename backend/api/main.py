0428d9e35e18492a1baaf94cb93da2c5c9b1af5d
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2026-06-12 15:33:07 +0200

    [+] connection TCP/IP to robot & send command to robot route

---
+++ backend/api/main.py
@@ -0,0 +1,32 @@
+import asyncio
+import os
+from dotenv import load_dotenv
+from fastapi import FastAPI
+
+load_dotenv()
+app = FastAPI()
+
+robot_reader: asyncio.StreamReader = None
+robot_writer: asyncio.StreamWriter = None
+
+@app.on_event("startup")
+async def connect_to_robot():
+    global robot_reader, robot_writer
+    try:
+        robot_reader, robot_writer = await asyncio.open_connection(os.getenv("ROBOT_HOST"), os.getenv("ROBOT_PORT"))
+        print("Connected to Robot successfully")
+    except Exception as e:
+        print(f"Failed to connect to Robot: {e}")
+
+async def send_robot_command(command: str):
+    if robot_writer:
+        robot_writer.write(f"{command}\n".encode())
+        await robot_writer.drain()
+        response = await robot_reader.readline()
+        return response.decode()
+    return "Robot non connecté"
+
+@app.post("/robot")
+async def send_command():
+    res = await send_robot_command("test")
+    return {"status": "success", "robot_response": res}

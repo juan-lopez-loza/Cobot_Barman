37f1ae1d6a45f7e40997d999ac373367f0a03a9f
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2026-06-15 10:41:01 +0200

    [+] create db + db schema

---
+++ backend/database/db.py
@@ -0,0 +1,45 @@
+import os
+from typing import Optional, List
+from sqlmodel import Field, Relationship, SQLModel, create_engine, Session
+
+sqlite_file_name = "database.db"
+sqlite_url = f"sqlite:///{sqlite_file_name}"
+
+engine = create_engine(sqlite_url, echo=True)
+
+class Cocktail(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    name: str = Field(max_length=50)
+
+    steps: list["RecipeStep"] = Relationship(back_populates="cocktail")
+
+class RecipeStep(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    cocktail_id: int = Field(foreign_key="cocktail.id")
+    step_order: int
+    description: str
+
+    cocktail: Cocktail = Relationship(back_populates="steps")
+    points: list["TrajectoryPoint"] = Relationship(back_populates="step")
+
+class TrajectoryPoint(SQLModel, table=True):
+    id: int = Field(default=None, primary_key=True)
+    step_id: int = Field(foreign_key="recipestep.id")
+    point_order: int
+    x: float
+    y: float
+    z: float
+    rx: float
+    ry: float
+    rz: float
+    speed: int = Field(default=10)
+    duration: int = Field(default=0)
+
+    step: RecipeStep = Relationship(back_populates="points")
+
+def init_db():
+    SQLModel.metadata.create_all(engine)
+
+def get_session():
+    with Session(engine) as session:
+        yield session

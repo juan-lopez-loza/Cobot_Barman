a279e2c4b67f05ece9e399cfeb2a9e3829d3c2eb
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2026-06-15 12:25:58 +0200

    [FIX] split connexion to robot

---
+++ backend/utils/robot.py
@@ -0,0 +1,21 @@
+import asyncio
+import os
+
+robot_reader: asyncio.StreamReader = None
+robot_writer: asyncio.StreamWriter = None
+
+async def connect_to_robot():
+    global robot_reader, robot_writer
+    try:
+        robot_reader, robot_writer = await asyncio.open_connection(os.getenv("ROBOT_HOST"), os.getenv("ROBOT_PORT"))
+        print("Connected to Robot successfully")
+    except Exception as e:
+        print(f"Failed to connect to Robot: {e}")
+
+async def send_robot_command(command: str):
+    if robot_writer:
+        robot_writer.write(f"{command}\n".encode())
+        await robot_writer.drain()
+        response = await robot_reader.readline()
+        return response.decode()
+    return "Robot non connecté"

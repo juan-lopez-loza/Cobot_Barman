fbc0547fa3c06c0fe4c8628a7cc48ac587753575
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2026-06-15 12:25:40 +0200

    [FIX] split main

--- backend/api/main.py
+++ backend/api/main.py
@@ -1,18 +1,12 @@
-import asyncio
-import os
-from dotenv import load_dotenv
 from fastapi import FastAPI
-from pydantic import BaseModel
-from database.db import (init_db)
+from dotenv import load_dotenv
+from database.db import init_db
+from utils.robot import connect_to_robot
+from api import orders, cocktails, admin
 
 load_dotenv()
 app = FastAPI()
 
-robot_reader: asyncio.StreamReader = None
-robot_writer: asyncio.StreamWriter = None
-
-class RobotCommand(BaseModel):
-    command: str
 
 @app.on_event("startup")
 async def startup():
@@ -21,24 +15,6 @@ async def startup():
 
     await connect_to_robot()
 
-async def connect_to_robot():
-    global robot_reader, robot_writer
-    try:
-        robot_reader, robot_writer = await asyncio.open_connection(os.getenv("ROBOT_HOST"), os.getenv("ROBOT_PORT"))
-        print("Connected to Robot successfully")
-    except Exception as e:
-        print(f"Failed to connect to Robot: {e}")
-
-async def send_robot_command(command: str):
-    if robot_writer:
-        robot_writer.write(f"{command}\n".encode())
-        await robot_writer.drain()
-        response = await robot_reader.readline()
-        return response.decode()
-    return "Robot non connecté"
-
-@app.post("/robot")
-async def send_command(payload: RobotCommand):
-    value = payload.command
-    res = await send_robot_command(value)
-    return {"status": "success", "robot_response": res}
+app.include_router(orders.router)
+# app.include_router(cocktails.router)
+# app.include_router(admin.router)

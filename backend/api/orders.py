5fbb52d46d8080477b8257668d3884b6472433cc
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2026-06-17 15:30:57 +0200

    [+] start order route

--- backend/api/orders.py
+++ backend/api/orders.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from utils.robot import send_robot_command
+from api.cocktails import get_db
+from database.db import engine
+from sqlmodel import Session, select
 
 router = APIRouter(prefix="/orders", tags=["orders"])
 
@@ -12,3 +15,8 @@ async def send_command(payload: RobotCommand):
     value = payload.command
     res = await send_robot_command(value)
     return {"status": "success", "robot_response": res}
+
+#@router.post("/order/{cocktail_id}")
+#async def send_order(cocktail_id: int, db: Session = Depends(get_db)):
+#    for x in recipestep:
+#        if cocktail_id = x.

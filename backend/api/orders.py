af081e410106bce098039595934db24406247176
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2026-06-19 23:59:05 +0200

    [+] test send several point to UR with json test before db

--- backend/api/orders.py
+++ backend/api/orders.py
@@ -2,13 +2,19 @@ from fastapi import APIRouter, Depends
 from pydantic import BaseModel
 from utils.robot import send_robot_command
 from api.cocktails import get_db
-from database.db import engine
+from database.db import engine, Cocktail, RecipeStep, TrajectoryPoint, Drink, GlassSlot
 from sqlmodel import Session, select
+import json
 
 router = APIRouter(prefix="/orders", tags=["orders"])
 
 class RobotCommand(BaseModel):
     command: str
+
+with open('./database/test.json', 'r') as f:
+    json_data = f.read()
+    data = json.loads(json_data)
+
 
 @router.post("/robot")
 async def send_command(payload: RobotCommand):
@@ -16,7 +22,13 @@ async def send_command(payload: RobotCommand):
     res = await send_robot_command(value)
     return {"status": "success", "robot_response": res}
 
-#@router.post("/order/{cocktail_id}")
-#async def send_order(cocktail_id: int, db: Session = Depends(get_db)):
-#    for x in recipestep:
-#        if cocktail_id = x.
+@router.post("/{cocktail_id}")
+async def send_order(cocktail_id: int, db: Session = Depends(get_db)):
+    statement = select(Cocktail.name).where(Cocktail.id == cocktail_id)
+    results = db.exec(statement)
+    print(f"Vous avez commandé le cocktail: {results.all()}")
+    for element in data:
+        print(element["value"])
+        await send_robot_command(element["value"])
+
+        return

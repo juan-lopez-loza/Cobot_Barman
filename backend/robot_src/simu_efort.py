e5d639988556ec65e05ecfc302ac4c88588ff060
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2026-06-20 00:01:06 +0200

    [COMMENT] all test progam

--- backend/robot_src/simu_efort.py
+++ backend/robot_src/simu_efort.py
@@ -1,24 +1,24 @@
-import asyncio
-from dotenv import load_dotenv
-import os
+#import asyncio
+#from dotenv import load_dotenv
+#import os
 
-load_dotenv()
+#load_dotenv()
 
-host = os.getenv("ROBOT_HOST")
-port = os.getenv("ROBOT_PORT")
+#host = os.getenv("ROBOT_HOST")
+#port = os.getenv("ROBOT_PORT")
 
 
-async def handle_client(reader, writer):
-    while True:
-        data = await reader.readline()
-        if not data: break
-        command = data.decode().strip()
-        if command == "PING": response = "PONG"
-        else: response = "ERROR"
-        writer.write(f"{response}\n".encode())
-        await writer.drain()
+#async def handle_client(reader, writer):
+#    while True:
+#        data = await reader.readline()
+#        if not data: break
+#        command = data.decode().strip()
+#        if command == "PING": response = "PONG"
+#        else: response = "ERROR"
+#        writer.write(f"{response}\n".encode())
+#        await writer.drain()
 
-async def init_server():
-    server =  await asyncio.start_server(handle_client, host=host, port=port)
-    print(f"Robot Server started at {host}:{port}")
-    asyncio.create_task(server.serve_forever())
+#async def init_server():
+#    server =  await asyncio.start_server(handle_client, host=host, port=port)
+#    print(f"Robot Server started at {host}:{port}")
+#    asyncio.create_task(server.serve_forever())

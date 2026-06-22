b8aace088853c05bdd766cb555a64c1bdd57608a
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2026-06-22 14:02:45 +0200

    [+] print log and sleep time

--- backend/api/orders.py
+++ backend/api/orders.py
@@ -1,6 +1,7 @@
+import asyncio
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
-from utils.robot import send_robot_command
+from utils.robot import send_robot_command, get_robot_response
 from api.cocktails import get_db
 from database.db import engine, Cocktail, RecipeStep, TrajectoryPoint, Drink, GlassSlot
 from sqlmodel import Session, select
@@ -27,8 +28,8 @@ async def send_order(cocktail_id: int, db: Session = Depends(get_db)):
     statement = select(Cocktail.name).where(Cocktail.id == cocktail_id)
     results = db.exec(statement)
     print(f"Vous avez commandé le cocktail: {results.all()}")
-    for element in data:
-        print(element["value"])
-        await send_robot_command(element["value"])
 
-        return
+    for element in data:
+        await send_robot_command(element["value"])
+        print(f"Command send to robot: {element['label']}")
+        await asyncio.sleep(5)

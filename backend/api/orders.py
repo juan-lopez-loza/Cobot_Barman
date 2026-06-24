e9b904f711dd7c32689d41905fad362204cc3dc7
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2026-06-24 16:22:56 +0200

    [+] send order

--- backend/api/orders.py
+++ backend/api/orders.py
@@ -1,7 +1,7 @@
 import asyncio
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
-from utils.robot import send_robot_command, get_robot_response
+from utils.robot import send_robot_command, get_robot_response, find_drink, create_script, send_to_robot
 from api.cocktails import get_db
 from database.db import engine, Cocktail, RecipeStep, TrajectoryPoint, Drink, GlassSlot
 from sqlmodel import Session, select
@@ -24,12 +24,7 @@ async def send_command(payload: RobotCommand):
     return {"status": "success", "robot_response": res}
 
 @router.post("/{cocktail_id}")
-async def send_order(cocktail_id: int, db: Session = Depends(get_db)):
-    statement = select(Cocktail.name).where(Cocktail.id == cocktail_id)
-    results = db.exec(statement)
-    print(f"Vous avez commandé le cocktail: {results.all()}")
-
-    for element in data:
-        await send_robot_command(element["value"])
-        print(f"Command send to robot: {element['label']}")
-        await asyncio.sleep(5)
+async def send_order(drink_name: str):
+    positions = find_drink(drink_name)
+    script = create_script(positions)
+    send_to_robot(script)

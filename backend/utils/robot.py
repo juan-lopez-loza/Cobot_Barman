b495942a3d5c7cf6ae199e2ea69f90d4b8297fe3
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2026-06-24 16:20:24 +0200

    [+] use socket,  func connect to robot, send to robot, find drink, create script

--- backend/utils/robot.py
+++ backend/utils/robot.py
@@ -1,33 +1,56 @@
 import asyncio
 import os
 from dotenv import load_dotenv
+import socket
+from email.header import UTF8
+import json
 
 load_dotenv()
 
 host = os.getenv("ROBOT_HOST")
 port = os.getenv("ROBOT_PORT")
 
-robot_reader: asyncio.StreamReader = None
-robot_writer: asyncio.StreamWriter = None
+with open('../database/test.json', 'r') as f:
+    json_data = f.read()
+    data = json.loads(json_data)
 
-async def connect_to_robot():
-    print(f"Connecting to Robot at {host}:{port}")
-    global robot_reader, robot_writer
+with open("../robot_src/init_onrobot.script", "r") as f:
+    init_script = f.read()
+
+def connect_to_robot():
     try:
-        robot_reader, robot_writer = await asyncio.open_connection(host, port)
-        print("Connected to Robot successfully")
+        s = socket.create_connection(host, port)
+        print("Connected to Robot")
     except Exception as e:
         print(f"Failed to connect to Robot: {e}")
+    return s
+
+def send_to_robot(script: str):
+    with open("./database/Fullscript.script", "w") as f:
+        f.write(script)
+    s = connect_to_robot()
+    try:
+        s.sendall(script.encode("UTF8"))
+        print("Command sent successfully")
+    except Exception as e:
+        print(f"Failed to send command: {e}")
+
+def find_drink(command: str):
+    drinks = data[0]["drinks"]
+    for drink in drinks:
+        if drink["name"] == command:
+            current_drink = drink['name']
+            return current_drink["positions"]
 
 
-async def send_robot_command(command: str):
-    if robot_writer:
-        robot_writer.write(f"{command}\n".encode())
-        await robot_writer.drain()
-        return "Commande envoyée"
-    return "Robot non connecté"
+def create_script(positions: list):
+    command = ""
+    for element in positions:
+        command += f"  {element['value']}\n"
 
-async def get_robot_response():
-    if robot_reader:
-        data = await robot_reader.readline()
-        print(f"Robot response: {data}")
+    fullscript = init_script
+    fullscript += '\n  popup("Program from pc started")\n'
+    fullscript += command
+    fullscript += '  popup("Program from pc ended")\n'
+    fullscript += "end\n"
+    return fullscript

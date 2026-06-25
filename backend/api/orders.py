e9b73295dc747542e6957be42382a34af36af847
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2026-06-25 14:02:45 +0200

    [+] call func find glasses for send order

--- backend/api/orders.py
+++ backend/api/orders.py
@@ -1,7 +1,7 @@
 import asyncio
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
-from utils.robot import find_drink, create_script, send_to_robot
+from utils.robot import find_drink, create_script, send_to_robot, find_glasses
 from api.cocktails import get_db
 from database.db import engine, Cocktail, RecipeStep, TrajectoryPoint, Drink, GlassSlot
 from sqlmodel import Session, select
@@ -13,6 +13,7 @@ class RobotCommand(BaseModel):
 
 @router.post("/")
 async def send_order(drink_name: str):
+    glass = find_glasses()
     positions = find_drink(drink_name)
-    script = create_script(positions)
+    script = create_script(positions, glass)
     send_to_robot(script)

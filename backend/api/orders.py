2c8f4ee7f06b883e36e49ad4c4668b114df75f77
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2026-06-26 11:01:13 +0200

    [+] call func change glass state

--- backend/api/orders.py
+++ backend/api/orders.py
@@ -1,7 +1,7 @@
 import asyncio
 from fastapi import APIRouter, Depends
 from pydantic import BaseModel
-from utils.robot import find_drink, create_script, send_to_robot, find_glasses, rg_command
+from utils.robot import find_drink, create_script, send_to_robot, find_glasses, rg_command, change_glass_state
 from api.cocktails import get_db
 from database.db import engine, Cocktail, RecipeStep, TrajectoryPoint, Drink, GlassSlot
 from sqlmodel import Session, select
@@ -14,7 +14,7 @@ class RobotCommand(BaseModel):
 @router.post("/")
 async def send_order(drink_id: int):
     glass = find_glasses()
-    #change_glass_state(glass)
+    change_glass_state(glass)
     rg_positions = rg_command()
     drink_positions = find_drink(drink_id)
     script = create_script(drink_positions, glass, rg_positions)

ad666a7c40ca4ffa5478d490afa0fe3b0c8ec725
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2026-07-03 12:18:36 +0200

    [+] import start xml server

--- backend/api/main.py
+++ backend/api/main.py
@@ -4,6 +4,7 @@ from database.db import init_db
 from app.utils.robot import connect_to_robot
 from app.utils.load_json import open_data
 from api import orders, cocktails, admin, glasses
+from app.rpc.xmlrpc_server import start_xmlrpc_server
 load_dotenv()
 app = FastAPI()
 app.include_router(orders.router)
@@ -13,6 +14,7 @@ app.include_router(glasses.router)
 
 @app.on_event("startup")
 async def startup():
+    start_xmlrpc_server()
     init_db()
     open_data()
     connect_to_robot()

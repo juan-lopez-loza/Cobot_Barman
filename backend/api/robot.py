9c2e90a5ffe83f44118759007da5cdb5339367a6
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2026-07-06 11:17:01 +0200

    [+] route get robot status

---
+++ backend/api/robot.py
@@ -0,0 +1,8 @@
+from fastapi import APIRouter
+from app.rpc.ur import ur_methods
+
+router = APIRouter(prefix="/robot")
+
+@router.get("/status")
+async def get_robot_status():
+    return ur_methods.get_status()

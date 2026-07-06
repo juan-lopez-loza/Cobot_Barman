97df4784fcd5a718b0b99d52ab69b7196766fdeb
juan-lopez-loza <juan.lopez-loza@epitech.eu>
2026-07-06 14:52:11 +0200

    [+] tag for swagger

--- backend/api/robot.py
+++ backend/api/robot.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 from app.rpc.ur import ur_methods
 
-router = APIRouter(prefix="/robot")
+router = APIRouter(prefix="/robot", tags=["Robot"])
 
 @router.get("/status")
 async def get_robot_status():
